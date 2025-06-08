39d27d709d735836a5cfab40de763e74568b2bdf
Om Amar <emailomamar@gmail.com>
2025-06-09 04:01:05 +0530

    updated

--- main/app.py
+++ main/app.py
@@ -1,21 +1,26 @@
-from flask import Flask, request, url_for, jsonify
+from flask import Flask, request, url_for, jsonify, send_from_directory
 from flask_cors import CORS
 import os
+import base64
+import random
+
 if 'VERCEL' in os.environ:
     import main.generate
 else:
     import generate
-import base64
-import random
 
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-app = Flask(__name__, static_folder=os.path.join(BASE_DIR, 'static'))
+app = Flask(__name__)
 if 'VERCEL' in os.environ:
     UPLOAD_FOLDER = '/tmp/uploads'
 else:
-    UPLOAD_FOLDER = os.path.join(BASE_DIR, 'static/uploads')
+    UPLOAD_FOLDER = os.path.join(BASE_DIR, 'uploads')
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 CORS(app)
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(UPLOAD_FOLDER, filename)
 
 @app.route('/api', methods=['POST'])
 def index():
@@ -32,14 +37,12 @@ def index():
             filepath = os.path.join(UPLOAD_FOLDER, filename)
             with open(filepath, 'wb') as f:
                 f.write(image_data)
-            image_url = url_for('static', filename=f'uploads/{filename}', _external=True)
+            image_url = url_for('uploaded_file', filename=filename, _external=True)
     except:
         image_url = None
     d = jsonify(main.generate.output(question, image_url))
-    if filepath and os.path.exists(filepath):
-        os.remove(filepath)
+    print(image_url)
     return d
-
 
 if __name__ == '__main__':
     app.run(debug=True)
